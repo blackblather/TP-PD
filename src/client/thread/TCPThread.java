c2393d11e292e75dac5292ec401ea8591beb1000
joaom <joao.monteiro95@hotmail.com>
2019-11-24 04:24:36 +0000

    Created empty file

---
+++ src/client/thread/TCPThread.java
@@ -0,0 +1,16 @@
+package client.thread;
+
+import client.network.TCPService;
+
+public class TCPThread extends Thread {
+    private TCPService tcpService;
+
+    public TCPThread(){
+        //tcpService = new TCPService();
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
