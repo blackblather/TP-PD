03ac9aff0f12d2b7c4946972bf7b8205be8374cd
joaom <joao.monteiro95@hotmail.com>
2019-11-24 04:11:56 +0000

    Deleted files

--- src/server/communication/ICommunication.java
+++
@@ -1,8 +0,0 @@
-package server.communication;
-
-import java.io.IOException;
-
-public interface ICommunication<T>{
-    void Update(Object o);
-    void SendMsg(T msg) throws IOException, IllegalArgumentException;
-}
