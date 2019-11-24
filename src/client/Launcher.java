09d397b52474cd5db4ec608a65ee861e557e8cfa
joaom <joao.monteiro95@hotmail.com>
2019-11-24 04:22:42 +0000

    Defined starting point

--- src/client/Launcher.java
+++ src/client/Launcher.java
@@ -1,7 +1,28 @@
 package client;
 
-public class Launcher {
+import client.controller.ClientController;
+import client.view.Login;
+import common.observable.ObservableController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import server.controller.ServerController;
+
+public class Launcher extends Application {
+    private ObservableController controller = new ServerController();
+
     public static void main(String [] args){
-        System.out.println("LOL?");
+        //TODO: Conexão
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage window) {
+        //Set starting point here
+        Login login = new Login(controller, window);
+        login.Show();
+        Login login2 = new Login(controller, new Stage());
+        login2.Show();
+        Login login3 = new Login(controller, new Stage());
+        login3.Show();
     }
 }
