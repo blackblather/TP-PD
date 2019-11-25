39c18254a66df3345964db776c920b495d70b2f8
joaom <joao.monteiro95@hotmail.com>
2019-11-25 01:31:54 +0000

    Updated informative comments

--- src/server/Launcher.java
+++ src/server/Launcher.java
@@ -7,10 +7,11 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Launcher {
+    //6002 localhost 6003 localhost 6004
     //private final String multicastAddr = "224.0.0.120";
     //private final Integer multicastPort = 6001;
     public static void main(String [] args){
-        //Syntax: java server.communication.Launcher PORT-TCP IP-DS PORT-DS IP-DB PORT-DB
+        //Syntax: java server.Launcher PORT-TCP IP-DS PORT-DS IP-DB PORT-DB
         if(args.length != 5){
             System.out.println("Invalid syntax.\nExpected syntax: java server.communication.Launcher PORT-TCP IP-DS PORT-DS IP-DB PORT-DB");
             System.exit(0);
