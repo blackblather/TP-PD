0c40c9e267ad03eb3d70b689f91e958c2a1c7f32
joaom <joao.monteiro95@hotmail.com>
2019-11-28 19:20:30 +0000

    Created base file for Register View

---
+++ src/client/view/Register.java
@@ -0,0 +1,92 @@
+package client.view;
+
+import common.controller.Controller;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class Register extends View {
+
+    Register(Controller controller, Stage window) {
+        super(controller, window);
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Register");
+    }
+
+    @Override
+    public Scene GetScene() {
+        //Create root
+        FlowPane root = new FlowPane(Orientation.VERTICAL);
+
+        //Create scene
+        Scene scene = new Scene(root, 250, 170);
+        scene.setFill(Color.WHITE);
+
+        //Create root elements
+        VBox vBox = new VBox(10);
+        HBox hBox = new HBox(10);
+
+        //Stylizes vBox
+        vBox.setPrefWidth(scene.getWidth());
+        vBox.setPrefHeight(scene.getHeight());
+        vBox.setPadding(new Insets(10,10,10,10));
+
+        //Align hBox's content
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+
+        //Label - Username
+        Label lblUsername = new Label("Insert username:");
+
+        //Textbox - Username
+        TextField txtUsername = new TextField();
+
+        //Label - Password
+        Label lblPassword = new Label("Insert password:");
+
+        //Textbox - Password
+        TextField txtPassword = new TextField();
+
+        //Label - Password
+        Label lblPasswordConf = new Label("Confirm password:");
+
+        //Textbox - Password
+        TextField txtPasswordConf = new TextField();
+
+        //Button - Register
+        Button btnRegister = new Button("Register");
+        btnRegister.setOnAction(event -> {
+            controller.Register(null, txtUsername.getText(), txtPassword.getText(), txtPasswordConf.getText());
+        });
+
+        //Add children to hBox
+        hBox.getChildren().addAll(btnRegister);
+
+        //Add children to vBox
+        vBox.getChildren().addAll(lblUsername,
+                txtUsername,
+                lblPassword,
+                txtPassword,
+                lblPasswordConf,
+                txtPasswordConf,
+                hBox);
+
+        root.getChildren().add(vBox);
+
+        return scene;
+    }
+
+    @Override
+    public void OnAccountCreated(Object ref) {
+
+    }
+}
