0b6c3d665c14d1fd6e2c089abc07be275d115496
joaom <joao.monteiro95@hotmail.com>
2019-11-28 16:37:22 +0000

    Constructor now throws IOException

--- src/server/thread/ClientRunnable.java
+++ src/server/thread/ClientRunnable.java
@@ -3,6 +3,7 @@ package server.thread;
 import server.controller.ServerController;
 import server.network.TCPService;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class ClientRunnable implements Runnable {
@@ -12,7 +13,7 @@ public class ClientRunnable implements Runnable {
     private int[] connectedClients;
     private final Object connectedClientsLock;
 
-    ClientRunnable(Socket socket, ServerController controller, final Object connectedClientsLock, int[] connectedClients){
+    ClientRunnable(Socket socket, ServerController controller, final Object connectedClientsLock, int[] connectedClients) throws IOException {
         this.connectedClients = connectedClients;
         this.connectedClientsLock = connectedClientsLock;
         this.tcpService = new TCPService(socket, controller);
@@ -22,6 +23,7 @@ public class ClientRunnable implements Runnable {
     public void run() {
         tcpService.ReceiveMsg();
         DecrementConnectedClients();
+        System.out.println("Client connection closed.");
     }
 
     private void DecrementConnectedClients(){
