c1de0f7b1cece664e29c41636d77eca9253312e7
joaom <joao.monteiro95@hotmail.com>
2019-11-28 14:22:56 +0000

    Added comment to remember how to execute client via command line

--- src/client/Launcher.java
+++ src/client/Launcher.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
+    //java -cp "..\..\..\json-20190722.jar;..\..\..\javafx-sdk-11.0.2;." client.Launcher
     private Controller controller = new ClientController();
 
     public static void main(String [] args){
