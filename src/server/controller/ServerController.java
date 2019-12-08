1c4e4ba6d3bf17f8585fd28f0f7fc9de043c8688
joaom <joao.monteiro95@hotmail.com>
2019-12-08 17:44:38 +0000

    ServerController validates Login info and notifies observers

--- src/server/controller/ServerController.java
+++ src/server/controller/ServerController.java
@@ -4,10 +4,50 @@ import common.controller.Controller;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.*;
+
+/*NOTA: A implementação desta classe ServerController não tem em consideração questões de segurança, tais como:
+    -> SQLInjection
+    -> Hash & Salt das passwords dos utilizadores
+*/
 public class ServerController extends Controller {
+    //JDBC driver name and database URL
+    private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private final String DB_NAME = "tp-pd";
+    private final String DB_URL = "jdbc:mysql://localhost/" + DB_NAME;
+
+    //Database credentials
+    private final String USER = "root";
+    private final String PASS = "";
+
+    //Database vars
+    Connection conn = null;
+    Statement stmt = null;
 
     public ServerController(){
+        try{
+            //Register JDBC driver
+            Class.forName(JDBC_DRIVER);
 
+            //Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+/*        finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException ignored){}// nothing we can do
+
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try*/
     }
 
     private boolean IsValidJSONRRequest(JSONObject jsonObject){
@@ -38,10 +78,26 @@ public class ServerController extends Controller {
 
     @Override
     public synchronized void Login(Object ref, String username, String password) {
-        System.out.println("GOT LOGIN REQUEST:");
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password);
-        Notify(ref, NotificationType.invalidCredentials);
+        System.out.println("GOT LOGIN REQUEST:\nUsername: " + username + "\nPassword: " + password);
+            String query = "SELECT COUNT(*) as 'total' from users where username = '" + username + "' AND password = '" + password + "'";
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if(rs.next() && rs.getInt("total") == 1)
+                Notify(ref, NotificationType.successfulLogin);
+            else
+                Notify(ref, NotificationType.invalidCredentials);
+        } catch (SQLException e ) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
