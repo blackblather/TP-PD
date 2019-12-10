b185bb3dddd5ed9318063764ed44c03170dc1e8c
joaom <joao.monteiro95@hotmail.com>
2019-12-10 15:46:22 +0000

    Updated comment explaining how to run server from cmd line

--- src/server/Launcher.java
+++ src/server/Launcher.java
@@ -12,7 +12,7 @@ public class Launcher {
     //private final Integer multicastPort = 6001;
     public static void main(String [] args){
         //Syntax: java server.Launcher PORT-TCP IP-DS PORT-DS IP-DB PORT-DB
-        //java -cp "..\..\..\json-20190722.jar;." server.Launcher 6002 localhost 6003 localhost 6004
+        //java -cp "..\..\..\json-20190722.jar;..\..\..\mysql-connector-java-5.1.48\mysql-connector-java-5.1.48-bin.jar;." server.Launcher 6002 localhost 6003 localhost 6004
         if(args.length != 5){
             System.out.println("Invalid syntax.\nExpected syntax: java server.communication.Launcher PORT-TCP IP-DS PORT-DS IP-DB PORT-DB");
             System.exit(0);
