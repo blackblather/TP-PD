56d3f1183d009f0a3cdccf799f44910af2550e03
joaom <joao.monteiro95@hotmail.com>
2019-12-15 18:15:23 +0000

    Created base file for AddSong View. OnClick "Open File", FileChooser opens and after closing, gets the absolute path of the selected file

---
+++ src/client/view/AddSong.java
@@ -0,0 +1,96 @@
+package client.view;
+
+import common.controller.Controller;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.File;
+
+public class AddSong extends View {
+    AddSong(Controller controller, Stage window) {
+        super(controller, window);
+    }
+
+    @Override
+    public Scene GetScene() {
+        //Create root
+        FlowPane root = new FlowPane(Orientation.VERTICAL);
+
+        //Create scene
+        Scene scene = new Scene(root, 270, 230);
+        scene.setFill(Color.WHITE);
+
+        //Create root elements
+        VBox vBox = new VBox(10);
+        HBox hBox = new HBox(10);
+
+        //Stylizes vBox
+        vBox.setPrefWidth(scene.getWidth());
+        vBox.setPrefHeight(scene.getHeight());
+        vBox.setPadding(new Insets(10,10,10,10));
+
+        //Align hBox's content
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+
+        //Label - Song Name
+        Label lblSongName = new Label("Song name:");
+
+        //Textbox - Song Name
+        TextField txtSongName = new TextField();
+
+        //Label - File Path
+        Label lblFilePath = new Label("File path:");
+
+        //Textbox - File Path
+        TextField txtFilePath = new TextField();
+
+        //Button - Find File
+        Button btnFindFile = new Button("Open File");
+        btnFindFile.setOnAction(event -> {
+
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+                    new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+                    new FileChooser.ExtensionFilter("All Files", "*.*"));
+            File selectedFile = fileChooser.showOpenDialog(window);
+            if (selectedFile != null)
+                txtFilePath.setText(selectedFile.getAbsolutePath());
+        });
+
+        //Button - Add Song
+        Button btnAddSong = new Button("Add Song");
+        btnAddSong.setOnAction(event -> {
+
+        });
+
+        //Add children to hBox
+        hBox.getChildren().addAll(btnFindFile, btnAddSong);
+
+        //Add children to vBox
+        vBox.getChildren().addAll(lblSongName,
+                txtSongName,
+                lblFilePath,
+                txtFilePath,
+                hBox);
+
+        root.getChildren().add(vBox);
+
+        return scene;
+    }
+}
